61550d1be29b4f20a8a0c57b7fc18bd07a548970
Oliver Nederal <oliver.nederal@gmail.com>
2024-03-11 22:15:53 +0100

    Fixed the checkbox click behavior

--- src/components/checkboxList.tsx
+++ src/components/checkboxList.tsx
@@ -46,25 +46,25 @@ function checkboxList() {
       <ul>
         {tasks.map((task: Tasks) => {
           return (
+            // eslint-disable-next-line react/jsx-key
             <div className="inline-flex items-center justify-between w-full bg-white rounded-lg cursor-pointer peer-checked:bg-gray-100 peer-checked:text-gray-100 hover:text-gray-50 hover:bg-gray-50">
-              <div
-                onClick={() => handleCheckboxClick(task.id)}
-                className="w-full flex flex-row items-center p-1 cursor-pointer"
-              >
-                <input
-                  id={task.id}
-                  type="checkbox"
-                  className="bg-white border-green-300 focus:ring-3 focus:ring-green-300 h-full mx-1 rounded-full cursor-pointer"
-                  checked={task.completed}
-                  onChange={() => {}}
-                />
-                <label
-                  htmlFor={task.id}
-                  onClick={() => handleCheckboxClick(task.id)}
-                  className={`text-gray-700 cursor-pointer ${task.completed ? "line-through" : ""}`}
-                >
-                  {task.title}
-                </label>
+              <div className="w-full flex flex-row items-center p-1 cursor-pointer">
+                <div onClick={() => handleCheckboxClick(task.id)}>
+                  <input
+                    id={task.id}
+                    type="checkbox"
+                    className="bg-white border-green-300 focus:ring-3 focus:ring-green-300 h-full mx-1 rounded-full cursor-pointer"
+                    checked={task.completed}
+                    onChange={() => {}}
+                  />
+                  <label
+                    htmlFor={task.id}
+                    onClick={() => handleCheckboxClick(task.id)}
+                    className={`text-gray-700 cursor-pointer ${task.completed ? "line-through" : ""}`}
+                  >
+                    {task.title}
+                  </label>
+                </div>
               </div>
             </div>
           );
