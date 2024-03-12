1d241d144fffe05ac56accabb54703eb633b5b6f
Oliver Nederal <oliver.nederal@gmail.com>
2024-03-12 12:50:31 +0100

    Made the title bar sticky

--- src/tabs/Home.tsx
+++ src/tabs/Home.tsx
@@ -9,11 +9,11 @@ function Home() {
 
   const refreshCheckboxList = () => {
     setListKey(crypto.randomUUID());
-  }
+  };
 
   return (
     <div className="bg-white col-span-2 h-full w-full p-5 flex flex-col space-y-2">
-      <div className="flex flex-row space-x-5 items-center select-none p-1">
+      <div className="flex sticky top-5 flex-row space-x-5 items-center select-none p-1 bg-white bg-opacity-70">
         <h1 className="text-3xl font-bold">Home</h1>
         <div className="h-full w-px bg-gray-300" />
         <div className="flex flex-row items-center space-x-2">
@@ -23,13 +23,11 @@ function Home() {
             className="bg-gray-300 p-1 rounded-md"
             onClick={refreshCheckboxList}
           >
-            Cleanup Completed Tasks
+            Clean
           </button>
         </div>
       </div>
-      <CheckboxList
-        key={listKey}
-      />
+      <CheckboxList key={listKey} />
     </div>
   );
 }
