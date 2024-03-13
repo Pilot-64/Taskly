2039db6fe72d9e4b0495def4408b7c27e19a278b
MentoskaPraha <contact+git@mentoskapraha.com>
2024-03-13 10:04:44 +0200

    Extracted checkboxes into their own component

--- src/components/checkboxList.tsx
+++ src/components/checkboxList.tsx
@@ -1,6 +1,8 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import React, { useEffect } from "react";
-import { useState } from "react";
+import React from "react";
+import { useState, useEffect } from "react";
+
+import Checkbox from "./checkbox";
 
 function checkboxList() {
   const [tasks, setTasks] = useState<Tasks[]>([]);
@@ -39,14 +41,12 @@ function checkboxList() {
     }
   };
 
-  const handleCheckboxClick = (id: string) => {
-    setTasks((prevTasks) => {
-      const newTasks = prevTasks.map((item) =>
-        item.id == id ? { ...item, completed: !item.completed } : item
-      );
-      window.Main.SaveTasks(newTasks);
-      return newTasks;
-    });
+  const handleTaskUpdate = (updatedTask: Tasks) => {
+    const newTasks = tasks.map((item) =>
+      item.id == updatedTask.id ? updatedTask : item
+    );
+    setTasks(newTasks);
+    window.Main.SaveTasks(newTasks);
   };
 
   return (
@@ -54,30 +54,7 @@ function checkboxList() {
       <ul>
         {tasks.map((task: Tasks) => {
           return (
-            <div
-              key={task.id}
-              className="inline-flex items-center justify-between w-full bg-white rounded-lg cursor-pointer peer-checked:bg-gray-100 peer-checked:text-gray-100 hover:text-gray-50 hover:bg-gray-50"
-            >
-              <div
-                onClick={() => handleCheckboxClick(task.id)}
-                className="w-full flex flex-row items-center p-1 cursor-pointer"
-              >
-                <input
-                  id={task.id}
-                  type="checkbox"
-                  className="bg-white border-green-300 focus:ring-3 focus:ring-green-300 h-full mx-1 rounded-full cursor-pointer"
-                  checked={task.completed}
-                  onChange={() => {}}
-                />
-                <label
-                  htmlFor={task.id}
-                  onClick={() => handleCheckboxClick(task.id)}
-                  className={`text-gray-700 cursor-pointer ${task.completed ? "line-through" : ""}`}
-                >
-                  {task.title}
-                </label>
-              </div>
-            </div>
+            <Checkbox key={task.id} task={task} onUpdate={handleTaskUpdate} />
           );
         })}
       </ul>
