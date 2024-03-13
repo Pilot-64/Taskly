59bf7e14d509c35c6ebb50e352694e1beb6ae43a
MentoskaPraha <contact+git@mentoskapraha.com>
2024-03-13 11:49:29 +0200

    Added tab switching

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,39 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import React from "react";
+import { useState } from "react";
 
 import SideBar from "./components/sideBar";
 import Home from "./tabs/Home";
+import Today from "./tabs/Today";
+import Upcoming from "./tabs/Upcoming";
+import Settings from "./tabs/Settings";
+
+export enum Tabs {
+  home = "home",
+  today = "today",
+  upcoming = "upcoming",
+  settings = "settings"
+}
 
 function App() {
+  const [tab, setTab] = useState<Tabs>(Tabs.home);
+
+  const getTab = () => {
+    switch(tab){
+      case(Tabs.home): return(<Home/>);
+      case(Tabs.today): return(<Today/>);
+      case(Tabs.upcoming): return(<Upcoming/>);
+      case(Tabs.settings): return(<Settings/>);
+      default: return (<Home/>);
+    }
+  };
+
+  const handleTabSwitch = (newTab:Tabs) => setTab(newTab);
+
   return (
     <div className="font-taskly min-h-screen grid grid-cols-3 antialiased">
-      <SideBar />
-      <Home />
+      <SideBar onTabSwitch={handleTabSwitch} />
+      {getTab()}
     </div>
   );
 }
--- src/components/sideBar.tsx
+++ src/components/sideBar.tsx
@@ -1,8 +1,13 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import React from "react";
 import { IoHome, IoCalendar, IoSettingsSharp, IoToday } from "react-icons/io5";
+import { Tabs } from "../App";
 
-function SideBar() {
+interface sideBarProps {
+  onTabSwitch(newTab: string): void;
+}
+
+function SideBar({ onTabSwitch }: sideBarProps) {
   return (
     <div className="sticky top-0 bg-slate-100 h-dvh w-full p-4 self-start">
       <ul className="space-y-1 select-none">
@@ -11,17 +16,29 @@ function SideBar() {
           <p className="ml-3 font-medium">Lorem Ipsum Dolor</p>
         </div>
         <div className="w-full h-px bg-gray-300" />
-        <button className="p-2 bg-transparent hover:bg-sky-100 rounded-md flex items-center w-full">
+        <button
+          className="p-2 bg-transparent hover:bg-sky-100 rounded-md flex items-center w-full"
+          onClick={() => onTabSwitch(Tabs.home)}
+        >
           <IoHome className="mr-3 fill-slate-600" /> Home
         </button>
-        <button className="p-2 bg-transparent hover:bg-sky-100 rounded-md flex items-center w-full">
+        <button
+          className="p-2 bg-transparent hover:bg-sky-100 rounded-md flex items-center w-full"
+          onClick={() => onTabSwitch(Tabs.today)}
+        >
           <IoToday className="mr-3 fill-slate-600" /> Today
         </button>
-        <button className="p-2 bg-transparent hover:bg-sky-100 rounded-md flex items-center w-full">
+        <button
+          className="p-2 bg-transparent hover:bg-sky-100 rounded-md flex items-center w-full"
+          onClick={() => onTabSwitch(Tabs.upcoming)}
+        >
           <IoCalendar className="mr-3 fill-slate-600" /> Upcoming
         </button>
         <div className="w-full h-px bg-gray-300" />
-        <button className="p-2 bg-transparent hover:bg-sky-100 rounded-md flex items-center w-full">
+        <button
+          className="p-2 bg-transparent hover:bg-sky-100 rounded-md flex items-center w-full"
+          onClick={() => onTabSwitch(Tabs.settings)}
+        >
           <IoSettingsSharp className="mr-3 fill-slate-600" /> Settings
         </button>
       </ul>
