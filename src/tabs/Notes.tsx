3f79076683cd26b0032f91297c514ca88d346fb3
MentoskaPraha <contact+git@mentoskapraha.com>
2024-03-14 08:42:15 +0200

    Fixed UI look

--- src/tabs/Notes.tsx
+++ src/tabs/Notes.tsx
@@ -6,7 +6,6 @@ function Notes() {
     <div className="bg-white col-span-2 h-full w-full flex flex-col space-y-2">
       <div className="flex sticky top-0 flex-row space-x-5 p-5 items-center select-none bg-white bg-opacity-90">
         <h1 className="text-3xl font-bold">Notes</h1>
-        <div className="h-full w-px bg-gray-300" />
       </div>
       <div className="px-5">Hopefully Coming Soon!</div>
     </div>
