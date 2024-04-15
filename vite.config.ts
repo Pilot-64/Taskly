2f3702117ba51e2e871c95effdabcd149ddd2223
MentoskaPraha <contact+git@mentoskapraha.com>
2024-04-15 10:40:44 +0300

    Removed unneed = in comparisons

--- vite.config.ts
+++ vite.config.ts
@@ -45,7 +45,7 @@ export default ({ command }: ConfigEnv): UserConfig => {
       rollupOptions: {}
     },
     server: {
-      port: process.env.PORT === undefined ? 3000 : +process.env.PORT
+      port: process.env.PORT == undefined ? 3000 : +process.env.PORT
     },
     optimizeDeps: {
       exclude: ["path"]
